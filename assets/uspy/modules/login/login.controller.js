e4889e537232afb0c7e5abcab3285765e1660cdf
Ravy <smart.fz@mail.ru>
2018-01-31 21:18:31 +0300

    login BG

--- assets/uspy/modules/login/login.controller.js
+++ assets/uspy/modules/login/login.controller.js
@@ -5,13 +5,22 @@
         .module('uspy')
         .controller('loginController', loginController);
 
-    loginController.$inject = ['$http','config','authenticationService'];
+    loginController.$inject = ['$timeout','config','authenticationService'];
 
-    function loginController($http,config,authenticationService) {
+    function loginController($timeout,config,authenticationService) {
         let vm = this;
+        /**
+         * 1 - чувак входил и мы знаем его почту
+         * 2 - чувак впервые входит
+         * 3 - ввод пароля
+         * 4 - страница входа
+         */
+        vm.page = 1;
 
         vm.login = login;
         vm.reg = reg;
+        vm.switchPage = switchPage;
+        vm.goToFirst = goToFirst;
 
         activate();
         ///////////////////
@@ -26,6 +35,27 @@
         function reg(){
             authenticationService.reg(vm.regLogin, vm.regPass);
         }
+
+        /**
+         * Смена страницы окна
+         * @param page
+         */
+        function switchPage(page) {
+            $timeout(function(){
+                vm.page = page;
+            })
+        }
+
+        /**
+         * Переход на страниц ввода логина и пароля
+         */
+        function goToFirst() {
+            $timeout(function(){
+                vm.page = 2;
+            })
+        }
+
+
 
     }
 })();
